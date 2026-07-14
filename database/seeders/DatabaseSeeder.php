8ce01228d31c70286d47109d9a924ccc01d69b96
Elivelton <elivelton.cotrim@sixchains.com.br>
2026-07-13 22:19:32 -0300

    feat: atualiza seeding de usuários e organizadores para usar updateOrCreate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,43 +21,62 @@ class DatabaseSeeder extends Seeder
             UFSeeder::class,
         ]);
 
-        User::create([
-            'name'              => 'Admin',
-            'email'             => 'adm@example.com',
-            'email_verified_at' => now(),
-            'password'          => Hash::make('123456'),
-        ])->assignRole(RoleEnum::ADMIN->value);
+        User::updateOrCreate(
+            [
+                'email' => 'adm@example.com',
+            ],
+            [
+                'name' => 'Admin',
+                'email_verified_at' => now(),
+                'password' => Hash::make('123456'),
+            ]
+        )->assignRole(RoleEnum::ADMIN->value);
 
-        $userOrganizer = User::create([
-            'name'              => 'Organizer test',
-            'email'             => 'organizerTest@example.com',
+        $userOrganizer = User::updateOrCreate([
+            'email' => 'organizerTest@example.com',
+        ], [
+            'name' => 'Organizer test',
+            'email' => 'organizerTest@example.com',
             'email_verified_at' => now(),
-            'password'          => Hash::make('123456'),
+            'password' => Hash::make('123456'),
         ])->assignRole(RoleEnum::ORGANIZATION->value);
 
-        $organizer = Organizer::create([
-            'name'       => 'Organizer Test',
-            'phone'      => '999999999',
+        $organizer = Organizer::updateOrCreate([
+            'name' => 'Organizer Test',
+        ], [
+            'name' => 'Organizer Test',
+            'phone' => '999999999',
             'descrition' => 'Organizer Test Description',
         ]);
 
         $organizer->user()->save($userOrganizer);
 
-        $player = Player::create([
-            'phone'            => '999999999',
+        $player = Player::updateOrCreate([
+            'nickname' => 'PlayerTest'
+        ], [
+            'nickname' => 'PlayerTest',
+            'phone' => '999999999',
             'level_experience' => PlayerExperienceLevelEnum::BEGINNER->value,
-            'bio'              => 'Organizer Test bio',
-            'heart_team_name'  => 'Vasco',
-            'birth_dt'         => now()->format('Y-m-d'),
-            'sex'              => PlayerSexEnum::MALE->value,
-            'status'           => PlayerStatusEnum::ACTIVE->value,
-            'game_platform'    => PlayerPlatformGameEnum::PC->value,
+            'bio' => 'Organizer Test bio',
+            'heart_team_name' => 'Vasco',
+            'birth_dt' => now()->format('Y-m-d'),
+            'sex' => PlayerSexEnum::MALE->value,
+            'status' => PlayerStatusEnum::ACTIVE->value,
+            'game_platform' => PlayerPlatformGameEnum::PC->value,
         ]);
 
-        $player->user()->create([
-            'name'     => 'Player Test',
-            'email'    => 'player@mail.com',
+        $user = User::firstOrNew([
+            'email' => 'player@mail.com',
+        ]);
+
+        $user->fill([
+            'name' => 'Player Test',
             'password' => Hash::make('123456'),
-        ])->assignRole(RoleEnum::PLAYER->value);
+        ]);
+
+        $user->userable()->associate($player);
+        $user->save();
+
+        $user->assignRole(RoleEnum::PLAYER->value);
     }
 }
