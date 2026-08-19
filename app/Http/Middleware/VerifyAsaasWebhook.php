fa8f476266dfba80a8808ce9dae2ee302dc37a2a
Elivelton <elivelton.cotrim@sixchains.com.br>
2026-08-19 12:17:04 -0300

    feat: adiciona middleware para verificação de webhook da Asaas e configura rotas

---
+++ app/Http/Middleware/VerifyAsaasWebhook.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class VerifyAsaasWebhook
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Log::info('request-all', [
+            'assaasToken' => $request->header('asaas-access-token'),
+            'expectedToken' => config('asaas.sandbox.webhook_token'),
+            'ip' => $request->ip()
+        ]);
+
+        $asaasToken = $request->header('asaas-access-token');
+        $expectedToken = config('asaas.sandbox.webhook_token');
+
+        if (empty($asaasToken) || !hash_equals((string) $expectedToken, (string) $asaasToken)) {
+            abort(401, 'Token de autenticação inválido.');
+        }
+
+        $allowedIps = [
+            // Production
+            '52.67.12.206',
+            '18.230.8.159',
+            '54.94.136.112',
+            '54.94.183.101',
+
+            // SandBox
+            '54.233.45.238'
+        ];
+
+        if (!in_array($request->ip(), $allowedIps)) {
+            abort(403, 'IP não autorizado.');
+        }
+
+        return $next($request);
+    }
+}
--- config/asaas.php
+++ config/asaas.php
@@ -4,11 +4,13 @@ $version = env('ASAAS_API_VERSION');
 
 return [
     'sandbox' => [
-        'url'   => env('ASAAS_SANDBOX_URL') . '/' . $version,
+        'url' => env('ASAAS_SANDBOX_URL') . '/' . $version,
         'token' => env('ASAAS_SANDBOX_TOKEN'),
+        'webhook_token' => env('ASAAS_SANDBOX_WEBHOOK_TOKEN'),
     ],
     'production' => [
-        'url'   => env('ASAAS_PRODUCTION_URL') . '/' . $version,
+        'url' => env('ASAAS_PRODUCTION_URL') . '/' . $version,
         'token' => env('ASAAS_PRODUCTION_TOKEN'),
+        'webhook_token' => env('ASAAS_PRODUCTION_WEBHOOK_TOKEN'),
     ],
 ];
